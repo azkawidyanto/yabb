1951e67c268c1dbc0bf8fa0aed4fe8fc638edaf6
M Azka Widyanto <azkawidyanto@gmail.com>
2021-07-07 00:53:14 +0700

    Styling Html Pages

--- Homework/Week 1/Session 1/hw1.js
+++ Homework/Week 1/Session 1/hw1.js
@@ -1,3 +1,11 @@
 const test=()=>{
     console.log("test");
+    var title= document.forms["myForm"]["title"].value;
+    alert("Judul lagu adalah " +title);
+    document.getElementById("myForm").submit();
+    var div = document.getElementById('title-song');
+    div.innerHTML = div.innerHTML + document.forms["myForm"]["title"].value; 
 }
+
+
+
