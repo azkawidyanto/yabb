b4bd118f4feaed3bdaeda9a7982397bdd40f6419
M Azka Widyanto <azkawidyanto@gmail.com>
2021-07-06 19:28:46 +0700

    Restructur Folder

---
+++ Hands On/Week 1/Session1/test.js
@@ -0,0 +1,14 @@
+const students =[
+    {name:"Alice",age:"20"},
+    {name:"John". age:"20"},
+    {name:"bill",age:"20"},
+    {name:"james". age:"20"},
+]
+
+var table = document.createElement("table");   
+table.setAttribute( "name","age");
+var row = table.insertRow(students.length);
+
+for
+
+document.body.appendChild(table); 
