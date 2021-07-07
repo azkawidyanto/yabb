cb18662bc5f7bfbb2f196045ac4bac1c7f9cce20
M Azka Widyanto <azkawidyanto@gmail.com>
2021-07-07 08:42:37 +0700

    Change js funcrtion

--- Homework/Week 1/Session 1/hw1.js
+++ Homework/Week 1/Session 1/hw1.js
@@ -2,9 +2,11 @@ const test=()=>{
     console.log("test");
     var title= document.forms["myForm"]["title"];
     alert("Judul lagu adalah " +title.value);
+    
+    // var div = document.getElementById('title-song');
+    // div.innerHTML = title.value;
+    
     document.getElementById("myForm").submit();
-    var div = document.getElementById('title-song');
-    div.innerHTML = title.value;; 
 }
 
 
